68a4bd3f0eead5aa0683f533da58ae90003319a7
Jason Barr <jason@jasonsbarr.com>
2019-05-14 22:19:42 -0500

    Remove dummy todos from App state

--- src/App.js
+++ src/App.js
@@ -6,24 +6,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      todos: [
-        {
-          task: "Design website",
-          id: 111111111111,
-          completed: false,
-        },
-        {
-          task: "Buy food",
-          id: 222222222222,
-          completed: false,
-        },
-        {
-          task: "Get gas",
-          id: 333333333333,
-          completed: false,
-        },
-      ],
-
+      todos: [],
       todoText: "",
     };
   }
