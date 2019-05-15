9e8b844b8dfbe05efdae56082dc473f9f9d66a87
Jason Barr <jason@jasonsbarr.com>
2019-05-14 20:41:25 -0500

    Scaffold basic app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { Component } from "react";
 
-class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        {
+          task: "Design website",
+          id: 111111111111,
+          completed: false,
+        },
+        {
+          task: "Buy food",
+          id: 222222222222,
+          completed: false,
+        },
+        {
+          task: "Get gas",
+          id: 333333333333,
+          completed: false,
+        },
+      ],
+    };
+  }
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h1>Todo List</h1>
+        {/* todo items */}
+        {/* todo form */}
       </div>
     );
   }
