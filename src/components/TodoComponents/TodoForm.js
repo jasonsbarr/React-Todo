72f5f1758e5cb0e15ec14be4c3cd7a47b5cfd1c7
Jason Barr <jason@jasonsbarr.com>
2019-05-14 21:05:43 -0500

    Set up TodoForm

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React from "react";
 
 const TodoForm = props => {
   return (
-    // code here...
+    <form
+      onSubmit={props.onSubmit}
+      name="todoForm"
+      id="todoForm"
+      className="todoForm"
+    >
+      <input
+        onChange={props.onChange}
+        type="text"
+        name="addTodo"
+        id="addTodo"
+      />
+    </form>
   );
-}
+};
 
 export default TodoForm;
